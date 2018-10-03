266cf3c781ea68da594fbb4c5c79be4f79cca96c
jash <jash@schulterklopfer-productions.de>
2018-10-04 00:30:30 +0200

    created structure for configuring services inside a common docker container so the configuration process is os-independant

---
+++ install/bitcoind/script/trace.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+trace()
+{
+	if [ -n "${TRACING}" ]; then
+		echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] ${1}" > /dev/stderr
+	fi
+}
+
+trace_rc()
+{
+	if [ -n "${TRACING}" ]; then
+		echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Last return code: ${1}" > /dev/stderr
+	fi
+}
--- install/script/install.sh
+++ install/script/install.sh
@@ -2,6 +2,7 @@
 
 . ./trace.sh
 . ./docker.sh
+. ./cyphernodeconf.sh
 
 trace "Updating SatoshiPortal dockers"
 git submodule update --recursive --remote
@@ -15,3 +16,9 @@ build_docker_image ../SatoshiPortal/dockers/$arch/LN/c-lightning clnimg
 build_docker_image ../../cron_docker/ proxycronimg
 build_docker_image ../../proxy_docker/ btcproxyimg
 build_docker_image ../../pycoin_docker/ pycoinimg
+
+# build setup docker image
+build_docker_image ../ cyphernodeconf
+
+# configure bitcoind
+cyphernodeconf_configure bitcoind
