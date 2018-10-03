ac9b771bc4b21f04f50954ae17a328b096202f6b
jash <jash@schulterklopfer-productions.de>
2018-10-03 22:29:55 +0200

    added building of needed dockerfiles

---
+++ install/script/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. ./trace.sh
+. ./docker.sh
+
+trace "Updating SatoshiPortal dockers"
+git submodule update --recursive --remote
+
+# build SatoshiPortal images
+arch=x86_64
+build_docker_image ../SatoshiPortal/dockers/$arch/bitcoin-core btcnode
+build_docker_image ../SatoshiPortal/dockers/$arch/LN/c-lightning clnimg
+
+# build cyphernode images
+build_docker_image ../../cron_docker/ proxycronimg
+build_docker_image ../../proxy_docker/ btcproxyimg
+build_docker_image ../../pycoin_docker/ pycoinimg
