0111794380559ff1dddac75e8db9248448e34638
jash <jash@schulterklopfer-productions.de>
2018-10-06 18:43:14 +0200

    added generation of environment files for later installation step

--- install/generator-cyphernode/generators/app/features/electrum.js
+++ install/generator-cyphernode/generators/app/features/electrum.js
@@ -1,23 +1,32 @@
+const name = 'electrum';
 const featureCondition = function(props) {
-  return props.features && props.features.indexOf( 'electrum' ) != -1;
+  return props.features && props.features.indexOf( name ) != -1;
 }
 
-module.exports = function( utils ) {
-  return [{
-    when: featureCondition,
-    type: 'list',
-    name: 'electrum_implementation',
-    default: utils._getDefault( 'electrum_implementation' ),
-    message: 'What electrum implementation do you want to use?'+'\n',
-    choices: [
-      {
-        name: 'Electrum personal server',
-        value: 'eps'
-      },
-      {
-        name: 'Electrumx server',
-        value: 'elx'
-      }
-    ]
-  }];
+module.exports = {
+  name: function() { 
+    return name;
+  },
+  prompts: function( utils ) {
+    return [{
+      when: featureCondition,
+      type: 'list',
+      name: 'electrum_implementation',
+      default: utils._getDefault( 'electrum_implementation' ),
+      message: 'What electrum implementation do you want to use?'+'\n',
+      choices: [
+        {
+          name: 'Electrum personal server',
+          value: 'eps'
+        },
+        {
+          name: 'Electrumx server',
+          value: 'elx'
+        }
+      ]
+    }];
+  },
+  env: function( props ) {
+    return 'VAR0=VALUE0\nVAR1=VALUE1'
+  }
 };
--- install/generator-cyphernode/generators/app/features/lightning.js
+++ install/generator-cyphernode/generators/app/features/lightning.js
@@ -1,23 +1,32 @@
+const name = 'lightning';
 const featureCondition = function(props) {
-  return props.features && props.features.indexOf( 'lightning' ) != -1;
+  return props.features && props.features.indexOf( name ) != -1;
 }
 
-module.exports = function( utils ) {
-  return [{
-    when: featureCondition,
-    type: 'list',
-    name: 'lightning_implementation',
-    default: utils._getDefault( 'lightning_implementation' ),
-    message: 'What lightning implementation do you want to use?'+'\n',
-    choices: [
-      {
-        name: 'C-lightning',
-        value: 'c-lightning'
-      },
-      {
-        name: 'LND',
-        value: 'lnd'
-      }
-    ]
-  }];
+module.exports = {
+  name: function() { 
+    return name;
+  },
+  prompts: function( utils ) {
+    return [{
+      when: featureCondition,
+      type: 'list',
+      name: 'lightning_implementation',
+      default: utils._getDefault( 'lightning_implementation' ),
+      message: 'What lightning implementation do you want to use?'+'\n',
+      choices: [
+        {
+          name: 'C-lightning',
+          value: 'c-lightning'
+        },
+        {
+          name: 'LND',
+          value: 'lnd'
+        }
+      ]
+    }];
+  },
+  env: function( props ) {
+    return 'VAR0=VALUE0\nVAR1=VALUE1'
+  }
 };
