54f1d0461e2b73108ef7c57c12570fb5735baecf
jash <jash@schulterklopfer-productions.de>
2018-10-06 18:52:37 +0200

    fixed indentation

--- install/generator-cyphernode/generators/app/features/bitcoin.js
+++ install/generator-cyphernode/generators/app/features/bitcoin.js
@@ -4,27 +4,27 @@ const featureCondition = function(props) {
 };
 
 module.exports = {
-	name: function() { 
-		return name;
-	},
-	prompts: function( utils ) {
-		return [{
-	    when: featureCondition,
-	    type: 'confirm',
-	    name: 'bitcoin_prune',
-	    default: utils._getDefault( 'bitcoin_prune' ),
-	    message: 'Run bitcoin node in prune mode?'+'\n',
-	  },
-	  {
-	    when: featureCondition,
-	    type: 'input',
-	    name: 'bitcoin_external_ip',
-	    default: utils._getDefault( 'bitcoin_external_ip' ),
-	    validate: utils._ipValidator,
-	    message: 'What external ip does your bitcoin full node have?'+'\n',
-	  }];
-	},
-	env: function( props ) {
-		return 'VAR0=VALUE0\nVAR1=VALUE1'
-	}
+  name: function() { 
+    return name;
+  },
+  prompts: function( utils ) {
+    return [{
+      when: featureCondition,
+      type: 'confirm',
+      name: 'bitcoin_prune',
+      default: utils._getDefault( 'bitcoin_prune' ),
+      message: 'Run bitcoin node in prune mode?'+'\n',
+    },
+    {
+      when: featureCondition,
+      type: 'input',
+      name: 'bitcoin_external_ip',
+      default: utils._getDefault( 'bitcoin_external_ip' ),
+      validate: utils._ipValidator,
+      message: 'What external ip does your bitcoin full node have?'+'\n',
+    }];
+  },
+  env: function( props ) {
+    return 'VAR0=VALUE0\nVAR1=VALUE1'
+  }
 };
--- install/script/trace.sh
+++ install/script/trace.sh
@@ -2,14 +2,14 @@
 
 trace()
 {
-	if [ -n "${TRACING}" ]; then
-		echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] ${1}" > /dev/stderr
-	fi
+  if [ -n "${TRACING}" ]; then
+    echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] ${1}" > /dev/stderr
+  fi
 }
 
 trace_rc()
 {
-	if [ -n "${TRACING}" ]; then
-		echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Last return code: ${1}" > /dev/stderr
-	fi
+  if [ -n "${TRACING}" ]; then
+    echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Last return code: ${1}" > /dev/stderr
+  fi
 }
