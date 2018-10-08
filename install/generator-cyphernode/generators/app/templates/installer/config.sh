d94a8fef8ddc91aeae4929e679b218542dea68c4
jash <jash@schulterklopfer-productions.de>
2018-10-08 14:26:15 +0200

    renamed opentimestamps feature to otsclient

--- install/generator-cyphernode/generators/app/templates/installer/config.sh
+++ install/generator-cyphernode/generators/app/templates/installer/config.sh
@@ -1,5 +1,5 @@
 INSTALLER_MODE=<%= installer_mode %>
 BITCOIN_INTERNAL=<%= (bitcoin_mode==="internal"?'true':'false') %>
 FEATURE_LIGHTNING=<%= (features.indexOf('lightning') != -1)?'true':'false' %>
-FEATURE_OPENTIMESTAMPS=<%= (features.indexOf('opentimestamps') != -1)?'true':'false' %>
+FEATURE_OTSCLIENT=<%= (features.indexOf('otsclient') != -1)?'true':'false' %>
 FEATURE_ELECTRUM=<%= (features.indexOf('electrum') != -1)?'true':'false' %>
