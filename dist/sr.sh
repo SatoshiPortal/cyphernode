7e2bbbb2f7f424b2018a30960521ed19350eab4d
jash <jash@schulterklopfer-productions.de>
2018-10-13 13:26:43 +0200

    added a setup runner script

---
+++ dist/sr.sh
@@ -0,0 +1,1 @@
+curl -fsSL https://raw.githubusercontent.com/schulterklopfer/cyphernode/features/install/dist/setup.sh -o setup_cyphernode.sh && chmod +x setup_cyphernode.sh && ./setup_cyphernode.sh
