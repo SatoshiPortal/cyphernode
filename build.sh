398f5293735ea33b07d509cdc7e0bef44440d3c7
jash <jash@schulterklopfer-productions.de>
2018-10-14 15:06:33 +0200

    more verbose

--- build.sh
+++ build.sh
@@ -26,7 +26,8 @@ build_docker_image() {
   fi
 
   trace "building docker image: $2:latest"
-  docker build -q $1 -f $1/$dockerfile -t $2:latest > /dev/null
+  #docker build -q $1 -f $1/$dockerfile -t $2:latest > /dev/null
+  docker build $1 -f $1/$dockerfile -t $2:latest
 
 }
 
