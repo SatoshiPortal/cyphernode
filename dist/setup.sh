2912903972e0f4e681938e0bc2b95dee78e2d0ef
jash <jash@schulterklopfer-productions.de>
2018-10-14 13:20:56 +0200

    removed some bad characters from comment

--- dist/setup.sh
+++ dist/setup.sh
@@ -4,7 +4,7 @@
 # curl -fsSL get.docker.com -o get-docker.sh
 # sh get-docker.sh
 # sudo usermod -aG docker $USER
-## >>logout and relogin<<
+## logout and relogin
 # git clone --branch features/install --recursive https://github.com/schulterklopfer/cyphernode.git
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo chmod +x /usr/local/bin/docker-compose
