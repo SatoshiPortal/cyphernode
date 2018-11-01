b54fa802a0dd216857a9c480247bcbd5958828dc
jash <jash@schulterklopfer-productions.de>
2018-11-01 18:33:27 +0100

    run as user now works for gatekeeper. All keys and certs can be mode rw for the user which is used to run cyphernode.

--- api_auth_docker/entrypoint.sh
+++ api_auth_docker/entrypoint.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-spawn-fcgi -s /var/run/fcgiwrap.socket -u nginx -g nginx -U nginx -- `which fcgiwrap`
+user='nginx'
 
+if [[ $1 ]]; then
+	IFS=':' read -ra arr <<< "$1"
+
+	if [[ ${arr[0]} ]]; then
+		user=${arr[0]};
+	fi
+
+fi
+
+# create files with -rw-rw----
+# this will allow /var/run/fcgiwrap.socket to be accessed rw for group
+su -c "umask 0006" $user
+
+spawn-fcgi -M 0660 -s /var/run/fcgiwrap.socket -u $user -g nginx -U $user -- `which fcgiwrap`
 nginx -g "daemon off;"
