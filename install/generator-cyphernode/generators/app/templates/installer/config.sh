210d9aa5bf6866bd8a85e962a85b3d6f0464d680
jash <jash@schulterklopfer-productions.de>
2018-11-03 12:32:08 +0100

    removed opentimestamps feature

--- install/generator-cyphernode/generators/app/templates/installer/config.sh
+++ install/generator-cyphernode/generators/app/templates/installer/config.sh
@@ -1,7 +1,6 @@
 INSTALLER_MODE=<%= installer_mode %>
 BITCOIN_INTERNAL=<%= (bitcoin_mode==="internal"?'true':'false') %>
 FEATURE_LIGHTNING=<%= (features.indexOf('lightning') != -1)?'true':'false' %>
-FEATURE_OTSCLIENT=<%= (features.indexOf('otsclient') != -1)?'true':'false' %>
 FEATURE_ELECTRUM=<%= (features.indexOf('electrum') != -1)?'true':'false' %>
 LIGHTNING_IMPLEMENTATION=<%= lightning_implementation %>
 BITCOIN_DATAPATH=<%= bitcoin_datapath %>
