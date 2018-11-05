92c9d72deb9c4510ecc5bff35a9ec81c6d54dbb8
jash <jash@schulterklopfer-productions.de>
2018-11-05 21:58:31 +0100

    cert creation now takes CNs as argument for which the cert is valid

--- install/generator-cyphernode/generators/app/lib/cert.js
+++ install/generator-cyphernode/generators/app/lib/cert.js
@@ -3,32 +3,89 @@ const spawn = require('child_process').spawn;
 const defaultArgs = ['req', '-x509', '-newkey', 'rsa:4096', '-nodes'];
 const path = require('path');
 const tmp = require('tmp');
+const validator = require('validator');
+
+const confTmpl = `
+[req]
+distinguished_name = req_distinguished_name
+x509_extensions = v3_ca
+prompt = no
+[req_distinguished_name]
+CN = %PRIMARY_CN%
+[v3_ca]
+subjectAltName = @alt_names
+[alt_names]
+%ALT_DOMAINS%
+%ALT_IPS%
+`;
+
+const domainTmpl = 'DNS.%#% = %DOMAIN%';
+const ipTmpl = 'IP.%#% = %IP%'
 
 module.exports = class Cert {
 
 	constructor( options ) {
 		options = options || {};
-		this.args = options.args || { subj: '/CN=localhost', days: 3650 };
+		this.args = options.args || { days: 3650 };
 	}
 
-	async create() {
+	buildConfig( cns ) {
+
+		let ips = [];
+		let domains = [];
+
+		for( let cn of cns ) {
+			if( validator.isIP(cn) ) {
+				ips.push( cn );
+			} else {
+				domains.push( cn );
+			}
+		}
+
+		let conf = confTmpl;
+
+		if( !domains.length ) {
+			domains.push('localhost');
+		}
+
+		conf = conf.replace( '%PRIMARY_CN%', domains[0] )
+
+		let domainCount = 0;
+		domains = domains.map( d => domainTmpl.replace( '%#%', ++domainCount ).replace('%DOMAIN%', d) );
+		conf = conf.replace( '%ALT_DOMAINS%', domains.join('\n') || '' )
+
+		let ipCount = 0;
+		ips = ips.map( ip => ipTmpl.replace( '%#%', ++ipCount ).replace('%IP%', ip) );
+		conf = conf.replace( '%ALT_IPS%', ips.join('\n') || '' )
+
+		return conf;
+	}
+
+	async create( cns ) {
+		cns = cns || [];
 
 		let args = defaultArgs.slice();
 
 		const certFileTmp = tmp.fileSync();
 		const keyFileTmp = tmp.fileSync();
+		const confFileTmp = tmp.fileSync();
 
 		args.push( '-out' );
 		args.push( certFileTmp.name );
 		args.push( '-keyout' );
 		args.push( keyFileTmp.name );
+		args.push( '-config' );
+		args.push( confFileTmp.name );
 
 		for( let k in this.args ) {
 			args.push( '-'+k);
 			args.push( this.args[k] );
 		}
 
-		const openssl = spawn('openssl', args,  { stdio: ['ignore','ignore','ignore'] } );
+		const conf = this.buildConfig( cns );
+		fs.writeFileSync( confFileTmp.name, conf );
+		
+		const openssl = spawn('openssl', args,  { stdio: ['ignore', 'ignore', 'ignore'] } );
 		
 		let code = await new Promise( function(resolve, reject) {
 			openssl.on('exit', (code) => {
@@ -41,6 +98,7 @@ module.exports = class Cert {
 
 		certFileTmp.removeCallback();
 		keyFileTmp.removeCallback();
+		confFileTmp.removeCallback();
 
 		return {
 			code: code,
