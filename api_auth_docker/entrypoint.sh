dfe28b17794cd7ae933186e80e40c0d53abf1d78
kexkey <github@kexkey.com>
2018-11-19 21:39:54 -0500

    Tweaks and versioning and nginx arm alpine

--- api_auth_docker/entrypoint.sh
+++ api_auth_docker/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-spawn-fcgi -s /var/run/fcgiwrap.socket -u nginx -g nginx -U nginx -- /usr/bin/fcgiwrap
+spawn-fcgi -s /var/run/fcgiwrap.socket -u nginx -g nginx -U nginx -- `which fcgiwrap`
 
 nginx -g "daemon off;"
