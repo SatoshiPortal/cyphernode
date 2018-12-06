d7dad3216b38d26c1807f0afcfebd5b2f548f49f
jash <jash@schulterklopfer-productions.de>
2018-12-06 19:45:49 +0100

    only write stuff to config files, when its actually there

--- install/generator-cyphernode/generators/app/templates/installer/config.sh
+++ install/generator-cyphernode/generators/app/templates/installer/config.sh
@@ -3,11 +3,17 @@ BITCOIN_INTERNAL=<%= (bitcoin_mode==="internal"?'true':'false') %>
 FEATURE_LIGHTNING=<%= (features.indexOf('lightning') != -1)?'true':'false' %>
 FEATURE_OTSCLIENT=<%= (features.indexOf('otsclient') != -1)?'true':'false' %>
 LIGHTNING_IMPLEMENTATION=<%= lightning_implementation %>
-BITCOIN_DATAPATH=<%= bitcoin_datapath %>
-LIGHTNING_DATAPATH=<%= lightning_datapath %>
-OTSCLIENT_DATAPATH=<%= otsclient_datapath %>
 PROXY_DATAPATH=<%= proxy_datapath %>
 GATEKEEPER_DATAPATH=<%= gatekeeper_datapath %>
 DOCKER_MODE=<%= docker_mode %>
 RUN_AS_USER=<%= run_as_different_user?username:'' %>
 CLEANUP=<%= installer_cleanup?'true':'false' %>
+<% if ( features.indexOf('lightning') !== -1 && lightning_implementation === 'c-lightning' ) { %>
+OTSCLIENT_DATAPATH=<%= otsclient_datapath %>
+<% } %>
+<% if ( features.indexOf('otsclient') !== -1 ) { %>
+LIGHTNING_DATAPATH=<%= lightning_datapath %>
+<% } %>
+<% if ( bitcoin_mode==="internal" ) { %>
+BITCOIN_DATAPATH=<%= bitcoin_datapath %>
+<% } %>
