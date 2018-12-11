4d68783d14f8743add27128c8ceda0766fdfafab
kexkey <github@kexkey.com>
2018-12-11 16:51:17 -0500

    Added installation and features tests

--- install/generator-cyphernode/generators/app/templates/installer/start.sh
+++ install/generator-cyphernode/generators/app/templates/installer/start.sh
@@ -9,3 +9,10 @@ docker stack deploy -c docker-compose.yaml cyphernode
 <% } else if(docker_mode == 'compose') { %>
 docker-compose -f docker-compose.yaml up -d --remove-orphans
 <% } %>
+
+# Will test if Cyphernode is fully up and running...
+docker run --rm -it -v `pwd`/testfeatures.sh:/testfeatures.sh \
+-v `pwd`/gatekeeper/keys.properties:/keys.properties \
+-v `pwd`/gatekeeper/cert.pem:/cert.pem \
+-v <%= proxy_datapath %>:/proxy \
+--network cyphernodenet alpine:3.8 /testfeatures.sh
