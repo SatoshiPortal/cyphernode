039c8d0b03856b03d2abc18863c5ffb9ef5fcbaf
kexkey <github@kexkey.com>
2018-12-13 11:32:09 -0500

    Docs and ability to run when not directly in dist folder...

--- dist/sr.sh
+++ dist/sr.sh
@@ -1,1 +1,1 @@
-curl -fsSL https://raw.githubusercontent.com/schulterklopfer/cyphernode/features/install/dist/setup.sh -o setup_cyphernode.sh && chmod +x setup_cyphernode.sh && ./setup_cyphernode.sh
+curl -fsSL https://raw.githubusercontent.com/SatoshiPortal/cyphernode/master/dist/setup.sh -o setup_cyphernode.sh && chmod +x setup_cyphernode.sh && ./setup_cyphernode.sh
--- install/generator-cyphernode/generators/app/templates/installer/start.sh
+++ install/generator-cyphernode/generators/app/templates/installer/start.sh
@@ -3,16 +3,17 @@
 # run as user <%= username %>
 export USER=$(id -u <%= run_as_different_user?username:default_username %>):$(id -g <%= run_as_different_user?username:default_username %>)
 export ARCH=$(uname -m)
+current_path="$(cd "$(dirname "$0")" >/dev/null && pwd)"
 
 <% if (docker_mode == 'swarm') { %>
-docker stack deploy -c docker-compose.yaml cyphernode
+docker stack deploy -c $current_path/docker-compose.yaml cyphernode
 <% } else if(docker_mode == 'compose') { %>
-docker-compose -f docker-compose.yaml up -d --remove-orphans
+docker-compose -f $current_path/docker-compose.yaml up -d --remove-orphans
 <% } %>
 
 # Will test if Cyphernode is fully up and running...
-docker run --rm -it -v `pwd`/testfeatures.sh:/testfeatures.sh \
--v `pwd`/gatekeeper/keys.properties:/keys.properties \
--v `pwd`/gatekeeper/cert.pem:/cert.pem \
+docker run --rm -it -v $current_path/testfeatures.sh:/testfeatures.sh \
+-v $current_path/gatekeeper/keys.properties:/keys.properties \
+-v $current_path/gatekeeper/cert.pem:/cert.pem \
 -v <%= proxy_datapath %>:/proxy \
 --network cyphernodenet alpine:3.8 /testfeatures.sh
