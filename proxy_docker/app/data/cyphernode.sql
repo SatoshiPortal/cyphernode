bc032f702f2c14b56ff4bf323977a97823a40db3
kexkey <github@kexkey.com>
2018-12-13 14:41:26 -0500

    SQL migration

--- proxy_docker/app/data/cyphernode.sql
+++ proxy_docker/app/data/cyphernode.sql
@@ -63,3 +63,15 @@ CREATE TABLE stamp (
   calledback INTEGER DEFAULT FALSE,
   inserted_ts INTEGER DEFAULT CURRENT_TIMESTAMP
 );
+CREATE INDEX idx_stamp_hash ON stamp (hash);
+CREATE INDEX idx_stamp_calledback ON stamp (calledback);
+
+CREATE TABLE cyphernode_props (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  property TEXT,
+  value TEXT,
+  inserted_ts INTEGER DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_cp_property ON cyphernode_props (property);
+
+INSERT INTO cyphernode_props (property, value) VALUES ("version", "0.1");
