8b4b334cf0a70931c958907a55359a5d0ff7718d
kexkey <github@kexkey.com>
2019-01-08 16:01:49 -0500

    First pass of LN payment callbacks

--- proxy_docker/app/script/startproxy.sh
+++ proxy_docker/app/script/startproxy.sh
@@ -45,4 +45,6 @@ chmod 0600 $DB_FILE
 createCurlConfig ${WATCHER_BTC_NODE_RPC_CFG} ${WATCHER_BTC_NODE_RPC_USER}
 createCurlConfig ${SPENDER_BTC_NODE_RPC_CFG} ${SPENDER_BTC_NODE_RPC_USER}
 
+./waitanyinvoice.sh &
+
 nc -vlkp${PROXY_LISTENING_PORT} -e ./requesthandler.sh
