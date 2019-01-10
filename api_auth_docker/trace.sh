a8f7cfb2ef89ee1df83795b8c36ff7b4c0738c9b
kexkey <github@kexkey.com>
2019-01-10 10:05:24 -0500

    Added PID in traces

--- api_auth_docker/trace.sh
+++ api_auth_docker/trace.sh
@@ -3,13 +3,13 @@
 trace()
 {
 	if [ -n "${TRACING}" ]; then
-		echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] ${1}" 1>&2
+		echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] $$ ${1}" 1>&2
 	fi
 }
 
 trace_rc()
 {
 	if [ -n "${TRACING}" ]; then
-		echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Last return code: ${1}" 1>&2
+		echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] $$ Last return code: ${1}" 1>&2
 	fi
 }
