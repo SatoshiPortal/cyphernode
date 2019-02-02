3a7bf7eaf5170695e2ce23283f36d52dbb7db21c
kexkey <github@kexkey.com>
2019-02-01 23:40:58 -0500

    ln_pay timeout and sparkwallet testfeatures fix

--- install/generator-cyphernode/generators/app/templates/installer/testfeatures.sh
+++ install/generator-cyphernode/generators/app/templates/installer/testfeatures.sh
@@ -124,8 +124,8 @@ checksparkwallet() {
   echo -en "\r\n\e[1;36mTesting Spark Wallet... " > /dev/console
   local rc
 
-  rc=$(curl -s -o /dev/null -w "%{http_code}" https://gatekeeper/sparkwallet)
-  [ "${rc}" -ne "200" ] && return 400
+  rc=$(curl -s -o /dev/null -k -w "%{http_code}" https://gatekeeper/sparkwallet/)
+  [ "${rc}" -ne "401" ] && return 400
 
   echo -e "\e[1;36mSpark Wallet rocks!" > /dev/console
 
