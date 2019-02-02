142a41bcb535ea2bad4af9cbc5be539be017d7c5
kexkey <github@kexkey.com>
2019-02-02 11:51:11 -0500

    Migration based on feature, not version

--- proxy_docker/app/data/sqlmigrate20190104_0.1-0.2.sh
+++ proxy_docker/app/data/sqlmigrate20190104_0.1-0.2.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+echo "Checking for full LN support in DB..."
 exists=$(sqlite3 db/proxydb "SELECT value FROM cyphernode_props WHERE property='pay_index'")
 if [ -z "${exists}" ]; then
 	# pay_index not found, let's migrate
-	echo "Migrating database from v0.1 to v0.2..."
+	echo "Migrating database for full LN support..."
 	cat sqlmigrate20190104_0.1-0.2.sql | sqlite3 $DB_FILE
 else
-	echo "Database v0.1 to v0.2 migration already done, skipping!"
+	echo "Database full LN support migration already done, skipping!"
 fi
