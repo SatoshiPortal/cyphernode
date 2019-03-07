11f8891eeab6edb42dfc2c913c1c713352484bec
kexkey <github@kexkey.com>
2019-03-07 12:44:03 -0500

    Backups when migrating DB

--- proxy_docker/app/data/sqlmigrate20181213_0-0.1.sh
+++ proxy_docker/app/data/sqlmigrate20181213_0-0.1.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 echo "Checking for OTS support in DB..."
-sqlite3 db/proxydb ".tables" | grep "stamp" > /dev/null
+sqlite3 $DB_FILE ".tables" | grep "stamp" > /dev/null
 if [ "$?" -eq "1" ]; then
 	# stamp not there, we have to migrate
 	echo "Migrating database for OTS support..."
+  echo "Backing up current DB..."
+  cp  $DB_FILE $DB_FILE-sqlmigrate20181213_0-0.1
+  echo "Altering DB..."
 	cat sqlmigrate20181213_0-0.1.sql | sqlite3 $DB_FILE
 else
 	echo "Database OTS support migration already done, skipping!"
