a6551060466086ce3dedb2138aeb88d45d0039c9
kexkey <github@kexkey.com>
2019-03-07 12:44:03 -0500

    Backups when migrating DB

--- proxy_docker/app/data/sqlmigrate20190130_0.1-0.2.sh
+++ proxy_docker/app/data/sqlmigrate20190130_0.1-0.2.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 echo "Checking for watch by xpub support in DB..."
-sqlite3 db/proxydb ".tables" | grep "watching_by_pub32" > /dev/null
+sqlite3 $DB_FILE ".tables" | grep "watching_by_pub32" > /dev/null
 if [ "$?" -eq "1" ]; then
 	# watching_by_pub32 not there, we have to migrate
 	echo "Migrating database for watch by xpub support..."
+  echo "Backing up current DB..."
+  cp  $DB_FILE $DB_FILE-sqlmigrate20190130_0.1-0.2
+  echo "Altering DB..."
 	cat sqlmigrate20190130_0.1-0.2.sql | sqlite3 $DB_FILE
 else
 	echo "Database watch by xpub support migration already done, skipping!"
