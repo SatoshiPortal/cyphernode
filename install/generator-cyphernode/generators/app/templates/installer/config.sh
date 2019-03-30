ae12273dacb588737767fde02f718541bfea0e24
SKP <jash@schulterklopfer-productions.de>
2019-03-30 23:02:25 +0100

    traefik is now core feature

--- install/generator-cyphernode/generators/app/templates/installer/config.sh
+++ install/generator-cyphernode/generators/app/templates/installer/config.sh
@@ -1,17 +1,15 @@
 INSTALLER_MODE=<%= installer_mode %>
 BITCOIN_INTERNAL=<%= (bitcoin_mode==="internal"?'true':'false') %>
-FEATURE_TRAEFIK=<%= (features.indexOf('traefik') != -1)?'true':'false' %>
 FEATURE_LIGHTNING=<%= (features.indexOf('lightning') != -1)?'true':'false' %>
 FEATURE_OTSCLIENT=<%= (features.indexOf('otsclient') != -1)?'true':'false' %>
 LIGHTNING_IMPLEMENTATION=<%= lightning_implementation %>
 PROXY_DATAPATH=<%= proxy_datapath %>
 GATEKEEPER_DATAPATH=<%= gatekeeper_datapath %>
+TRAEFIK_DATAPATH=<%= traefik_datapath %>
 DOCKER_MODE=<%= docker_mode %>
 RUN_AS_USER=<%= run_as_different_user?username:'' %>
 CLEANUP=<%= installer_cleanup?'true':'false' %>
-<% if ( features.indexOf('traefik') !== -1 ) { %>
-TRAEFIK_DATAPATH=<%= traefik_datapath %>
-<% } %>
+SHARED_HTPASSWD_PATH=<%= traefik_datapath %>/htpasswd
 <% if ( features.indexOf('lightning') !== -1 && lightning_implementation === 'c-lightning' ) { %>
 LIGHTNING_DATAPATH=<%= lightning_datapath %>
 <% } %>
