d3ddf9780471c616864d89c59364efea17aedfa2
SKP <jash@schulterklopfer-productions.de>
2019-04-07 13:45:36 +0200

    Some cleanup in welcome app starting.

--- dist/apps/welcome/start.sh
+++ dist/apps/welcome/start.sh
@@ -1,16 +1,13 @@
-
-echo "SCRIPT_NAME: $SCRIPT_NAME"
-echo "SHARED_HTPASSWD_PATH: $SHARED_HTPASSWD_PATH"
-echo "APP_SCRIPT_PATH: $APP_SCRIPT_PATH"
-echo "APP_START_SCRIPT_PATH: $APP_START_SCRIPT_PATH"
-echo "GATEKEEPER_DATAPATH: $GATEKEEPER_DATAPATH"
+# APP_SCRIPT_PATH
+# APP_START_SCRIPT_PATH
+# APP_ID
 
 export SHARED_HTPASSWD_PATH
 export GATEKEEPER_DATAPATH
 export APP_SCRIPT_PATH
 
 if [ "$DOCKER_MODE" = "swarm" ]; then
-  docker stack deploy -c $APP_SCRIPT_PATH/docker-compose.yaml cn_welcome
+  docker stack deploy -c $APP_SCRIPT_PATH/docker-compose.yaml $APP_ID
 elif [ "$DOCKER_MODE" = "compose" ]; then
   docker-compose -f $APP_SCRIPT_PATH/docker-compose.yaml up -d --remove-orphans
 fi
