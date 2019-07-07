79654c604c23a743d618ed2eb042c4d9d5b75972
SKP <jash@schulterklopfer-productions.de>
2019-07-08 01:02:55 +0200

    added wasabi stuff to setup.sh

--- cyphernodeconf_docker/templates/installer/config.sh
+++ cyphernodeconf_docker/templates/installer/config.sh
@@ -2,6 +2,7 @@ INSTALLER_MODE=<%= installer_mode %>
 BITCOIN_INTERNAL=<%= (bitcoin_mode==="internal"?'true':'false') %>
 FEATURE_LIGHTNING=<%= (features.indexOf('lightning') != -1)?'true':'false' %>
 FEATURE_OTSCLIENT=<%= (features.indexOf('otsclient') != -1)?'true':'false' %>
+FEATURE_WASABI=<%= (features.indexOf('wasabi') != -1)?'true':'false' %>
 LIGHTNING_IMPLEMENTATION=<%= lightning_implementation %>
 PROXY_DATAPATH=<%= proxy_datapath %>
 GATEKEEPER_DATAPATH=<%= gatekeeper_datapath %>
