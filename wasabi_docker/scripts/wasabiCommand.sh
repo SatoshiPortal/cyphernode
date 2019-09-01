b830202ee15cb914d9fffdb2afa6af0da20df8a6
kexkey <github@kexkey.com>
2019-09-01 10:52:21 -0400

    Removed --mixall so it doesn't remix already enough mixed coins

--- wasabi_docker/scripts/wasabiCommand.sh
+++ wasabi_docker/scripts/wasabiCommand.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 tor &
-dotnet WalletWasabi.Gui.dll mix --wallet:$1 --mixall --keepalive
+#dotnet WalletWasabi.Gui.dll mix --wallet:$1 --mixall --keepalive
+dotnet WalletWasabi.Gui.dll mix --wallet:$1 --keepalive
