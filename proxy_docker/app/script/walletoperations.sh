ee2142a56a89d6a3b4047927009d19cc1ac736cc
SKP <skp@skp.rocks>
2020-01-18 15:05:31 +0100

    Added disable_private_key param to create_wallet

--- proxy_docker/app/script/walletoperations.sh
+++ proxy_docker/app/script/walletoperations.sh
@@ -395,11 +395,17 @@ batchspend() {
 }
 
 create_wallet() {
+  # creates a wallet without private key
   trace "[Entering create_wallet()]"
 
   local walletname=${1}
+  local disable_private_keys='true'
 
-  local rpcstring="{\"method\":\"createwallet\",\"params\":[\"${walletname}\",true]}"
+  if [ ${2} == 'false' ]; then
+      disable_private_keys='false'
+  fi
+
+  local rpcstring="{\"method\":\"createwallet\",\"params\":[\"${walletname}\",${disable_private_keys}]}"
   trace "[create_wallet] rpcstring=${rpcstring}"
 
   local result
