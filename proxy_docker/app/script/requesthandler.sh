adbad9c0fed912ce08bcc88960ad538d7a53589f
kexkey <github@kexkey.com>
2020-02-06 11:11:20 -0500

    elements_validateaddress

--- proxy_docker/app/script/requesthandler.sh
+++ proxy_docker/app/script/requesthandler.sh
@@ -760,7 +760,7 @@ main() {
           ;;
         elements_spend)
           # POST http://192.168.111.152:8080/elements_spend
-          # BODY {"address":"bcrt1q0k88aheqyj02uz5x38p4cgf7grfcc6hpacctgd","amount":0.00233,"assetId":"bc5ac68d102a16069c68de127773473eee0a6bc760689ce76024a3cfbfec31cf","eventMessage":"eyJ3aGF0ZXZlciI6MTIzfQo="}
+          # BODY {"address":"AzpmavTHCTfJhUqoS28kg3aTmCzu9uqCdfkqmpCALetAoa3ERpZnHvhNzjMP3wo4XitKEMm62mjFk7B9","amount":0.00233,"assetId":"bc5ac68d102a16069c68de127773473eee0a6bc760689ce76024a3cfbfec31cf","eventMessage":"eyJ3aGF0ZXZlciI6MTIzfQo="}
 
           response=$(elements_spend "${line}")
           response_to_client "${response}" ${?}
@@ -770,6 +770,13 @@ main() {
           # curl (GET) 192.168.111.152:8080/elements_getwalletinfo
 
           response=$(elements_getwalletinfo)
+          response_to_client "${response}" ${?}
+          break
+          ;;
+        elements_validateaddress)
+          # GET http://192.168.111.152:8080/elements_validateaddress/AzpmavTHCTfJhUqoS28kg3aTmCzu9uqCdfkqmpCALetAoa3ERpZnHvhNzjMP3wo4XitKEMm62mjFk7B9
+
+          response=$(elements_validateaddress $(echo "${line}" | cut -d ' ' -f2 | cut -d '/' -f3))
           response_to_client "${response}" ${?}
           break
           ;;
