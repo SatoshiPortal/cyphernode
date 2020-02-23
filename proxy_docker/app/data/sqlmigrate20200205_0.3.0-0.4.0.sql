26b095ce11774596a68ecc6988e5abdd0a811fba
kexkey <github@kexkey.com>
2020-02-23 17:58:32 -0500

    SQL migration for elements

--- proxy_docker/app/data/sqlmigrate20200205_0.3.0-0.4.0.sql
+++ proxy_docker/app/data/sqlmigrate20200205_0.3.0-0.4.0.sql
@@ -1,16 +1,31 @@
 PRAGMA foreign_keys = ON;
 
+CREATE TABLE elements_watching_by_pub32 (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  pub32 TEXT UNIQUE,
+  label TEXT UNIQUE,
+  derivation_path TEXT,
+  callback0conf TEXT,
+  callback1conf TEXT,
+  last_imported_n INTEGER,
+  watching INTEGER DEFAULT FALSE,
+  inserted_ts INTEGER DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE elements_watching (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   address TEXT UNIQUE,
+  unblinded_address TEXT UNIQUE,
   watching INTEGER DEFAULT FALSE,
   callback0conf TEXT,
   calledback0conf INTEGER DEFAULT FALSE,
   callback1conf TEXT,
   calledback1conf INTEGER DEFAULT FALSE,
   imported INTEGER DEFAULT FALSE,
+  watching_by_pub32_id INTEGER REFERENCES elements_watching_by_pub32,
+  pub32_index INTEGER,
   event_message TEXT,
-  assetid TEXT,
+  watching_assetid TEXT,
   inserted_ts INTEGER DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -18,7 +33,8 @@ CREATE TABLE elements_watching_tx (
   elements_watching_id INTEGER REFERENCES elements_watching,
   elements_tx_id INTEGER REFERENCES elements_tx,
   vout INTEGER,
-  amount REAL
+  amount REAL,
+  assetid TEXT
 );
 CREATE UNIQUE INDEX idx_elements_watching_tx ON elements_watching_tx (elements_watching_id, elements_tx_id);
 
@@ -31,12 +47,11 @@ CREATE TABLE elements_tx (
   fee REAL,
   size INTEGER,
   vsize INTEGER,
-	is_replaceable INTEGER,
+  is_replaceable INTEGER,
   blockhash TEXT,
   blockheight INTEGER,
   blocktime INTEGER,
   raw_tx TEXT,
-  assetid TEXT,
   inserted_ts INTEGER DEFAULT CURRENT_TIMESTAMP
 );
 CREATE INDEX idx_elements_tx_timereceived ON elements_tx (timereceived);
@@ -46,13 +61,29 @@ CREATE INDEX idx_elements_tx_vsize ON elements_tx (vsize);
 CREATE INDEX idx_elements_tx_blockhash ON elements_tx (blockhash);
 CREATE INDEX idx_elements_tx_blockheight ON elements_tx (blockheight);
 CREATE INDEX idx_elements_tx_blocktime ON elements_tx (blocktime);
-CREATE INDEX idx_elements_tx_assetid ON elements_tx (assetid);
 
 CREATE TABLE elements_recipient (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   address TEXT,
+  unblinded_address TEXT,
   amount REAL,
+  assetid TEXT,
   tx_id INTEGER REFERENCES elements_tx,
   inserted_ts INTEGER DEFAULT CURRENT_TIMESTAMP
 );
 CREATE INDEX idx_elements_recipient_address ON elements_recipient (address);
+CREATE INDEX idx_elements_recipient_unblinded_address ON elements_recipient (unblinded_address);
+CREATE INDEX idx_elements_recipient_assetid ON elements_recipient (assetid);
+
+CREATE TABLE elements_watching_by_txid (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  txid TEXT UNIQUE,
+  watching INTEGER DEFAULT FALSE,
+  callback1conf TEXT,
+  calledback1conf INTEGER DEFAULT FALSE,
+  callbackxconf TEXT,
+  calledbackxconf INTEGER DEFAULT FALSE,
+  nbxconf INTEGER,
+  inserted_ts INTEGER DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_elements_watching_by_txid_txid ON elements_watching_by_txid (txid);
