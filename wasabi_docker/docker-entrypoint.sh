0c528c6a89d8a81f1df180090f3c3d14865bb072
kexkey <github@kexkey.com>
2020-04-09 18:18:41 -0400

    Adapted for new version of Wasabi, graceful stop and select wallet

--- wasabi_docker/docker-entrypoint.sh
+++ wasabi_docker/docker-entrypoint.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env sh
 
+termi() {
+  echo "SIGTERM or SIGINT detected!"
+
+  local dotnetpid=$(pidof dotnet)
+  local torpid=$(pidof tor)
+  echo "dotnetpid=${dotnetpid}"
+  echo "torpid=${torpid}"
+
+  kill -TERM ${dotnetpid} ${torpid}
+  echo "Waiting for dotnet and tor to end..."
+
+  while [ -e /proc/${dotnetpid} ] && [ -e /proc/${torpid} ]; do sleep 1; done
+}
+
+trap termi TERM INT
+
+trim() {
+	echo -e "$1" | sed -e 's/^[[:space:]]*//' | sed -e 's/[[:space:]]*$//'
+}
+
+user=$( trim ${WASABI_RPC_USER} )
+echo "user=${user}" > ${WASABI_RPC_CFG}
+
 wallet_name=${WALLET_NAME:-wasabi}
 
 # check if we have a wallet file
@@ -12,8 +35,11 @@ fi
 /app/scripts/checkWalletPassword.sh $wallet_name ""
 
 if [ $? = 0 ]; then
-  exec /app/scripts/startWasabi.sh $wallet_name ""
+  (while [ "${walletloaded}" != "true" ]; do sleep 5 ; echo "CYPHERNODE: Trying to load Wasabi Wallet..." ; curl -s --config ${WASABI_RPC_CFG} -d '{"jsonrpc":"2.0","id":"0","method":"selectwallet", "params":["wasabi"]}' localhost:18099 > /dev/null ; [ "$?" = "0" ] && walletloaded=true ; done ; echo "CYPHERNODE: Wasabi Wallet loaded!") &
+
+  /app/scripts/startWasabi.sh wasabi "" &
+  wait $!
+
 else
   echo "Wrong password"
 fi
-
