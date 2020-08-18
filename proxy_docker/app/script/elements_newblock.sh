d1a2e8eedb16ca2e4498a7ff74381ae4f41cedd7
kexkey <github@kexkey.com>
2020-08-18 17:34:11 -0400

    Better newblock handling for elements

--- proxy_docker/app/script/elements_newblock.sh
+++ proxy_docker/app/script/elements_newblock.sh
@@ -10,16 +10,16 @@ elements_newblock() {
   local request=${1}
   local blockhash=$(echo "${request}" | cut -d ' ' -f2 | cut -d '/' -f3)
 
-  local blockinfo
-  blockinfo=$(elements_get_block_info ${blockhash})
-
-  local blockheight
-  blockheight=$(echo ${blockinfo} | jq -r ".result.height")
-
-  trace "[elements_newblock] mosquitto_pub -h broker -t elements_newblock -m \"{\"blockhash\":\"${blockhash}\",\"blockheight\":\"${blockheight}\"}\""
-  response=$(mosquitto_pub -h broker -t elements_newblock -m "{\"blockhash\":\"${blockhash}\",\"blockheight\":\"${blockheight}\"}")
+  trace "[elements_newblock] mosquitto_pub -h broker -t elements_newblock -m \"{\"blockhash\":\"${blockhash}\"}\""
+  response=$(mosquitto_pub -h broker -t elements_newblock -m "{\"blockhash\":\"${blockhash}\"}")
   returncode=$?
   trace_rc ${returncode}
 
-  elements_do_callbacks_txid
+  # This seems to be called in some cases when catching up syncing and was flooding Cyphernode,
+  # so since blocks on Liquid are pretty fast anyway and there's no mempool overflow, we can skip
+  # blocks before checking for callbacks.
+  if [ "$(( $(od -An -N2 < /dev/urandom) % 5 ))" = "0" ]; then
+    trace "[elements_newblock] Let's see if we have webhooks to call"
+    elements_do_callbacks_txid
+  fi
 }
