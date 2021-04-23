e844b7539ce0069b4ba09eb01c59a80f3fed0659
kexkey <github@kexkey.com>
2021-04-22 20:10:18 -0400

    We don't launch a Tor proxy for Wasabi if Tor is already installed

--- wasabi_docker/docker-entrypoint.sh
+++ wasabi_docker/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-termi() {
+# Tor is not running as part of Cyphernode, try to kill it locally in here
+termi_wtor() {
   echo "SIGTERM or SIGINT detected!"
 
   local dotnetpid=$(pidof dotnet)
@@ -14,7 +15,26 @@ termi() {
   while [ -e /proc/${dotnetpid} ] && [ -e /proc/${torpid} ]; do sleep 1; done
 }
 
-trap termi TERM INT
+# Tor is running as part of Cyphernode, don't try to kill it locally in here
+termi_wotor() {
+  echo "SIGTERM or SIGINT detected!"
+
+  local dotnetpid=$(pidof dotnet)
+
+  echo "dotnetpid=${dotnetpid}"
+
+  kill -TERM ${dotnetpid}
+  echo "Waiting for dotnet to end..."
+
+  while [ -e /proc/${dotnetpid} ]; do sleep 1; done
+}
+
+# If TOR_HOST is defined, it means Tor has been installed in Cyphernode setup, use it!
+if [ -n "${TOR_HOST}" ]; then
+  trap termi_wotor TERM INT
+else
+  trap termi_wtor TERM INT
+fi
 
 trim() {
 	echo -e "$1" | sed -e 's/^[[:space:]]*//' | sed -e 's/[[:space:]]*$//'
@@ -35,6 +55,19 @@ fi
 /app/scripts/checkWalletPassword.sh $wallet_name ""
 
 if [ $? = 0 ]; then
+  # If TOR_HOST is defined, it means Tor has been installed in Cyphernode setup, use it!
+  if [ -n "${TOR_HOST}" ]; then
+    while [ -z "${TORIP}" ]; do echo "tor not ready" ; TORIP=$(getent hosts tor | awk '{ print $1 }') ; sleep 10 ; done
+    echo "tor ready at IP ${TORIP}"
+    cp /root/.walletwasabi/client/Config.json /root/.walletwasabi/client/Config-ori.json
+    # Wasabi needs an IP address as the Tor socks5 endpoint, unfortunately
+    jq --arg torip "${TORIP}:${TOR_PORT}" '.TorSocks5EndPoint = $torip' /root/.walletwasabi/client/Config-ori.json > /root/.walletwasabi/client/Config.json
+  else
+    echo "Tor will be launched locally with Wasabi"
+    cp /root/.walletwasabi/client/Config.json /root/.walletwasabi/client/Config-ori.json
+    jq --arg torip "127.0.0.1:9050" '.TorSocks5EndPoint = $torip' /root/.walletwasabi/client/Config-ori.json > /root/.walletwasabi/client/Config.json
+  fi
+
   (while [ "${walletloaded}" != "true" ]; do sleep 5 ; echo "CYPHERNODE: Trying to load Wasabi Wallet..." ; curl -s --config ${WASABI_RPC_CFG} -d '{"jsonrpc":"2.0","id":"0","method":"selectwallet", "params":["wasabi"]}' localhost:18099 > /dev/null ; [ "$?" = "0" ] && walletloaded=true ; done ; echo "CYPHERNODE: Wasabi Wallet loaded!") &
 
   /app/scripts/startWasabi.sh wasabi "" &
--- wasabi_docker/scripts/wasabiCommand.sh
+++ wasabi_docker/scripts/wasabiCommand.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
-tor &
+
+# If TOR_HOST is not defined, it means Tor has not been installed in Cyphernode setup,
+# let's launch a local instance!
+if [ -z "${TOR_HOST}" ]; then
+  tor &
+fi
+
 #dotnet WalletWasabi.Gui.dll mix --wallet:$1 --mixall --keepalive
 dotnet WalletWasabi.Gui.dll mix --wallet:$1 --keepalive
