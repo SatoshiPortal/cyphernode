3b797243d01c4bed5eb84acf6f19d0501a11852c
kexkey <github@kexkey.com>
2021-06-22 11:14:27 -0400

    v0.7.0-rc.2

--- docker-build.sh
+++ docker-build.sh
@@ -65,9 +65,9 @@ aarch64_docker="arm64"
 #arch_docker=${aarch64_docker}
 arch_docker=${x86_docker}
 
-v1="v0-rc.1"
-v2="v0.7-rc.1"
-v3="v0.7.0-rc.1"
+v1="v0-rc.2"
+v2="v0.7-rc.2"
+v3="v0.7.0-rc.2"
 
 echo "\nBuilding Cyphernode Core containers\n"
 echo "arch_docker=$arch_docker\n"
