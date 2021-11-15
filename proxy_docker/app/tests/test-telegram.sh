d3f11d9fdc247438f007020a77400150a360db8d
Philippe Lamy <philippe@philippelamy.ca>
2021-11-15 12:01:02 -0500

    move test in tests directory

--- proxy_docker/app/tests/test-telegram.sh
+++ proxy_docker/app/tests/test-telegram.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-. notify.sh
-
-TRACING=1
-
-export TRACING
+cd ..
+. ./notify.sh
 
 echo "Calling notify_telegram..."
 
 notify_telegram "{\"text\":\"Unit testing notify_telegram at `date -u +"%FT%H%MZ"`\"}"
 
 echo "Done..."
-
 
