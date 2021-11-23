4251a0c15352d0b9c400612dc7243ae37fb8248e
kexkey <github@kexkey.com>
2021-11-23 12:16:27 -0500

    cleaned validateaddress in requesthandler

--- proxy_docker/app/script/requesthandler.sh
+++ proxy_docker/app/script/requesthandler.sh
@@ -327,8 +327,7 @@ main() {
           # GET http://192.168.111.152:8080/validateaddress/tb1p5cyxnuxmeuwuvkwfem96lqzszd02n6xdcjrs20cac6yqjjwudpxqp3mvzv
 
           response=$(validateaddress $(echo "${line}" | cut -d ' ' -f2 | cut -d '/' -f3))
-          response_to_client "${response}" ${?}
-          break
+          returncode=$?
           ;;
         spend)
           # POST http://192.168.111.152:8080/spend
