9165e950e2c259e7a5d8caa6f4378a2a0cd40825
kexkey <github@kexkey.com>
2022-01-18 11:29:21 -0500

    v0.8.0-rc.2

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 
 TRACING=1
 
-# CYPHERNODE VERSION "v0.8.0-rc.1-dev" -> dev over v0.8.0-rc.1
-CONF_VERSION="v0.8.0-rc.1-dev-local"
-GATEKEEPER_VERSION="v0.8.0-rc.1-dev-local"
-TOR_VERSION="v0.8.0-rc.1-dev-local"
-PROXY_VERSION="v0.8.0-rc.1-dev-local"
-NOTIFIER_VERSION="v0.8.0-rc.1-dev-local"
-PROXYCRON_VERSION="v0.8.0-rc.1-dev-local"
-OTSCLIENT_VERSION="v0.8.0-rc.1-dev-local"
-PYCOIN_VERSION="v0.8.0-rc.1-dev-local"
+# CYPHERNODE VERSION "v0.8.0-rc.2-dev" -> dev over v0.8.0-rc.2
+CONF_VERSION="v0.8.0-rc.2-dev-local"
+GATEKEEPER_VERSION="v0.8.0-rc.2-dev-local"
+TOR_VERSION="v0.8.0-rc.2-dev-local"
+PROXY_VERSION="v0.8.0-rc.2-dev-local"
+NOTIFIER_VERSION="v0.8.0-rc.2-dev-local"
+PROXYCRON_VERSION="v0.8.0-rc.2-dev-local"
+OTSCLIENT_VERSION="v0.8.0-rc.2-dev-local"
+PYCOIN_VERSION="v0.8.0-rc.2-dev-local"
 
 trace()
 {
