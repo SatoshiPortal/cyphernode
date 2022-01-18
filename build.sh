0a8a560896f1c3c14ce6682f9c0eb5984fad5594
kexkey <github@kexkey.com>
2022-01-18 12:43:55 -0500

    Right image tags when building locally

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 
 TRACING=1
 
-# CYPHERNODE VERSION "v0.8.0-rc.2-dev" -> dev over v0.8.0-rc.2
-CONF_VERSION="v0.8.0-rc.2-dev-local"
-GATEKEEPER_VERSION="v0.8.0-rc.2-dev-local"
-TOR_VERSION="v0.8.0-rc.2-dev-local"
-PROXY_VERSION="v0.8.0-rc.2-dev-local"
-NOTIFIER_VERSION="v0.8.0-rc.2-dev-local"
-PROXYCRON_VERSION="v0.8.0-rc.2-dev-local"
-OTSCLIENT_VERSION="v0.8.0-rc.2-dev-local"
-PYCOIN_VERSION="v0.8.0-rc.2-dev-local"
+# CYPHERNODE VERSION "v0.8.0-rc.2"
+CONF_VERSION="v0.8.0-rc.2-local"
+GATEKEEPER_VERSION="v0.8.0-rc.2-local"
+TOR_VERSION="v0.8.0-rc.2-local"
+PROXY_VERSION="v0.8.0-rc.2-local"
+NOTIFIER_VERSION="v0.8.0-rc.2-local"
+PROXYCRON_VERSION="v0.8.0-rc.2-local"
+OTSCLIENT_VERSION="v0.8.0-rc.2-local"
+PYCOIN_VERSION="v0.8.0-rc.2-local"
 
 trace()
 {
