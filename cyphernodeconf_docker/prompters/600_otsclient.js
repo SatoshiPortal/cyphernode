ededc6e2c40efc73c67ea92becdb281889c6bad0
Philippe Lamy <philippe@philippelamy.ca>
2022-03-29 16:12:45 -0400

    Moved env variables out of docker-compose.yaml

---
+++ cyphernodeconf_docker/prompters/600_otsclient.js
@@ -0,0 +1,15 @@
+const chalk = require('chalk');
+
+const name = 'otsclient';
+
+module.exports = {
+  name: function() {
+    return name;
+  },
+  prompts: function( utils ) {
+    return [];
+  },
+  templates: function( props ) {
+    return [ 'otsclient.env' ];
+  }
+};
