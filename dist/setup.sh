e45f484fab23368efe1146acfedd406c9023d71f
kexkey <Kexkey@users.noreply.github.com>
2022-03-30 15:19:17 -0400

    Releases/v0.8.1 (#256)
    * v0.8.1
    
    * env_file dir permissions were too strict
    
    * wrong permissions take two

--- dist/setup.sh
+++ dist/setup.sh
@@ -127,7 +127,7 @@ sudo_if_required() {
 }
 
 modify_permissions() {
-  local directories=("$current_path/.env" "$BITCOIN_DATAPATH" "$LIGHTNING_DATAPATH" "$PROXY_DATAPATH" "$GATEKEEPER_DATAPATH" "$OTSCLIENT_DATAPATH" "$POSTGRES_DATAPATH" "$LOGS_DATAPATH" "$TRAEFIK_DATAPATH" "$TOR_DATAPATH")
+  local directories=("$BITCOIN_DATAPATH" "$LIGHTNING_DATAPATH" "$PROXY_DATAPATH" "$GATEKEEPER_DATAPATH" "$OTSCLIENT_DATAPATH" "$POSTGRES_DATAPATH" "$LOGS_DATAPATH" "$TRAEFIK_DATAPATH" "$TOR_DATAPATH")
   for d in "${directories[@]}"
   do
     if [[ -e $d ]]; then
@@ -139,7 +139,7 @@ modify_permissions() {
 }
 
 modify_owner() {
-  local directories=("$current_path/.env" "$BITCOIN_DATAPATH" "$LIGHTNING_DATAPATH" "$PROXY_DATAPATH" "$GATEKEEPER_DATAPATH" "$OTSCLIENT_DATAPATH" "$POSTGRES_DATAPATH" "$LOGS_DATAPATH" "$TRAEFIK_DATAPATH" "$TOR_DATAPATH")
+  local directories=("${current_path}/.env" "$BITCOIN_DATAPATH" "$LIGHTNING_DATAPATH" "$PROXY_DATAPATH" "$GATEKEEPER_DATAPATH" "$OTSCLIENT_DATAPATH" "$POSTGRES_DATAPATH" "$LOGS_DATAPATH" "$TRAEFIK_DATAPATH" "$TOR_DATAPATH")
   local user=$(id -u $RUN_AS_USER):$(id -g $RUN_AS_USER)
   for d in "${directories[@]}"
   do
@@ -475,8 +475,8 @@ install_docker() {
   fi
 
   if [ ! -d $current_path/.env ]; then
-    step "   [32mcreate[0m $current_path/.env"
-    sudo_if_required mkdir -p $current_path/.env
+    step "   [32mcreate[0m ${current_path}/.env"
+    sudo_if_required mkdir -p ${current_path}/.env
     next
   fi
 
@@ -484,7 +484,7 @@ install_docker() {
   copy_file $cyphernodeconf_filepath/cyphernode/info.json $PROXY_DATAPATH/info.json 1 $SUDO_REQUIRED
   copy_file $cyphernodeconf_filepath/postgres/pgpass $PROXY_DATAPATH/pgpass 1 $SUDO_REQUIRED
   sudo_if_required chmod 0600 $PROXY_DATAPATH/pgpass
-  copy_file $cyphernodeconf_filepath/proxycron/proxycron.env $current_path/.env/proxycron.env 1 $SUDO_REQUIRED
+  copy_file $cyphernodeconf_filepath/proxycron/proxycron.env ${current_path}/.env/proxycron.env 1 $SUDO_REQUIRED
 
   if [[ $BITCOIN_INTERNAL == true ]]; then
     if [ ! -d $BITCOIN_DATAPATH ]; then
@@ -676,7 +676,7 @@ install_docker() {
 
 check_directory_owner() {
   # if one directory does not have access rights for $RUN_AS_USER, we echo 1, else we echo 0
-  local directories=("$current_path/.env" "$BITCOIN_DATAPATH" "$LIGHTNING_DATAPATH" "$PROXY_DATAPATH" "$GATEKEEPER_DATAPATH" "$POSTGRES_DATAPATH" "$LOGS_DATAPATH" "$TRAEFIK_DATAPATH" "$TOR_DATAPATH")
+  local directories=("${current_path}/.env" "$BITCOIN_DATAPATH" "$LIGHTNING_DATAPATH" "$PROXY_DATAPATH" "$GATEKEEPER_DATAPATH" "$POSTGRES_DATAPATH" "$LOGS_DATAPATH" "$TRAEFIK_DATAPATH" "$TOR_DATAPATH")
   local status=0
   for d in "${directories[@]}"
   do
@@ -780,7 +780,7 @@ sanity_checks_pre_install() {
       if [[ $sudo_reason == 'directories' ]]; then
         echo "          [31mor check your data volumes if they have the right owner.[0m"
         echo "          [31mThe owner of the following folders should be '$RUN_AS_USER':[0m"
-        local directories=("$current_path/.env" "$BITCOIN_DATAPATH" "$LIGHTNING_DATAPATH" "$PROXY_DATAPATH" "$GATEKEEPER_DATAPATH" "$POSTGRES_DATAPATH" "$LOGS_DATAPATH" "$TRAEFIK_DATAPATH" "$TOR_DATAPATH")
+        local directories=("$BITCOIN_DATAPATH" "$LIGHTNING_DATAPATH" "$PROXY_DATAPATH" "$GATEKEEPER_DATAPATH" "$POSTGRES_DATAPATH" "$LOGS_DATAPATH" "$TRAEFIK_DATAPATH" "$TOR_DATAPATH")
           local status=0
           for d in "${directories[@]}"
           do
@@ -866,17 +866,17 @@ ALWAYSYES=0
 SUDO_REQUIRED=0
 AUTOSTART=0
 
-# CYPHERNODE VERSION "v0.9.0-dev"
-SETUP_VERSION="v0.9.0-dev"
-CONF_VERSION="v0.9.0-dev"
-GATEKEEPER_VERSION="v0.9.0-dev"
-TOR_VERSION="v0.9.0-dev"
-PROXY_VERSION="v0.9.0-dev"
-NOTIFIER_VERSION="v0.9.0-dev"
-PROXYCRON_VERSION="v0.9.0-dev"
-OTSCLIENT_VERSION="v0.9.0-dev"
-PYCOIN_VERSION="v0.9.0-dev"
-CYPHERAPPS_VERSION="dev"
+# CYPHERNODE VERSION "v0.8.1"
+SETUP_VERSION="v0.8.1"
+CONF_VERSION="v0.8.1"
+GATEKEEPER_VERSION="v0.8.1"
+TOR_VERSION="v0.8.1"
+PROXY_VERSION="v0.8.1"
+NOTIFIER_VERSION="v0.8.1"
+PROXYCRON_VERSION="v0.8.1"
+OTSCLIENT_VERSION="v0.8.1"
+PYCOIN_VERSION="v0.8.1"
+CYPHERAPPS_VERSION="v0.8.1"
 BITCOIN_VERSION="v22.0"
 LIGHTNING_VERSION="v0.10.2"
 TRAEFIK_VERSION="v1.7.9-alpine"
