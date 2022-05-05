cb5106119693a7bdec1a2191d0b3aaaf31d0dd93
Philippe Lamy <philippe@philippelamy.ca>
2022-05-05 11:54:34 -0400

    mine up to 101 blocks in regtest

--- cyphernodeconf_docker/templates/bitcoin/entrypoint.sh
+++ cyphernodeconf_docker/templates/bitcoin/entrypoint.sh
@@ -5,13 +5,17 @@ rm -f /container_monitor/bitcoin_ready
 <% if ( features.indexOf('tor') !== -1 && torifyables && torifyables.indexOf('tor_bitcoin') !== -1 ) { %>
 while [  ! -f "/container_monitor/tor_ready" ];
 do
-    echo "CYPHERNODE: Waiting for Tor to be ready before starting bitcoind"
+    echo "CYPHERNODE[entrypoint]: Waiting for Tor to be ready before starting bitcoind"
     sleep 10
 done
-echo "CYPHERNODE: Tor is ready - Starting bitcoind"
+echo "CYPHERNODE[entrypoint]: Tor is ready - Starting bitcoind"
 <% } %>
 
 # Create default wallets if they are not loaded
 /.bitcoin/createWallets.sh &
+
+<% if( net === 'regtest' ) { %>
+/.bitcoin/mine.sh &
+<% } %>
 
 exec bitcoind
