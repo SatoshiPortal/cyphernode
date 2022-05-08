c2f776e41ebaf0af042072daee38ad32bb3e0483
Philippe Lamy <philippe@philippelamy.ca>
2022-05-08 08:14:07 -0400

    moved getblock from proxy to btc node

--- cyphernodeconf_docker/templates/bitcoin/newblock.sh
+++ cyphernodeconf_docker/templates/bitcoin/newblock.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
-blockhash="$@"
-echo "[newblock] [$blockhash]"
-mosquitto_pub -h broker -t bitcoin_node_newblock -m "$blockhash"
+newblock(){
+  echo "[newblock] Enering newblock"
+
+  local blockhash="$@"
+  echo "[newblock] [blockhash=$blockhash]"
+
+  local blockheight
+  blockheight=$(get_block_height $blockhash)
+
+  echo "[newblock] mosquitto_pub -h broker -t newblock -m \"{\"blockhash\":\"${blockhash}\",\"blockheight\":${blockheight}}\""
+  mosquitto_pub -h broker -t newblock -m "{\"blockhash\":\"${blockhash}\",\"blockheight\":${blockheight}}"
+}
+
+get_block_height(){
+  local blockinfo
+  blockinfo=$(bitcoin-cli getblock $1)
+
+  local blockheight
+  blockheight=$(echo ${blockinfo} | jq -r ".height")
+
+  echo $blockheight
+}
+
+newblock $@
+
 echo "[newblock] Done"
