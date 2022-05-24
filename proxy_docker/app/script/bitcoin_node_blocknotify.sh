f5c28566b95c6bdc4ee5a5c41de25d843efb19d8
Philippe Lamy <philippe@philippelamy.ca>
2022-05-24 12:28:30 -0400

    rebase - changed newblock by blocknotify to be more core

--- proxy_docker/app/script/bitcoin_node_blocknotify.sh
+++ proxy_docker/app/script/bitcoin_node_blocknotify.sh
@@ -4,25 +4,25 @@
 . ./callbacks_txid.sh
 . ./batching.sh
 
-bitcoin_node_newblock() {
+bitcoin_node_blocknotify() {
   local blockheight
   local newblockheight
 
-  trace "Entering bitcoin_node_newblock()..."
+  trace "Entering bitcoin_node_blocknotify()..."
 
   while true  # Keep an infinite loop to reconnect when connection lost/broker unavailable
   do
-    trace "[bitcoin_node_newblock] mosquitto_sub --retained-only -h broker -t newblock -C 1 -W 5"
+    trace "[bitcoin_node_blocknotify] mosquitto_sub --retained-only -h broker -t newblock -C 1 -W 5"
     message=$(mosquitto_sub --retained-only -h broker -t newblock -C 1 -W 5) 
-    trace "[bitcoin_node_newblock] Message: ${message}"
+    trace "[bitcoin_node_blocknotify] Message: ${message}"
 
     newblockheight=$(echo $message | jq .blockheight)
     if [ -n "$newblockheight" ] && [ "$newblockheight" != "$blockheight" ]; then
       processNewBlock ${message}
       blockheight=$newblockheight
-      trace "[bitcoin_node_newblock] Done processing"
+      trace "[bitcoin_node_blocknotify] Done processing"
     fi
-    trace "[bitcoin_node_newblock] reconnecting in 60 secs" 
+    trace "[bitcoin_node_blocknotify] reconnecting in 60 secs" 
     sleep 60
   done
 }
@@ -31,7 +31,7 @@ processNewBlock(){
   (
   flock -x 202
 
-  trace "[bitcoin_node_newblock] Entering processNewblock()..."
+  trace "[bitcoin_node_blocknotify] Entering processNewblock()..."
 
   do_callbacks_txid
   batch_check_webhooks
@@ -39,7 +39,7 @@ processNewBlock(){
   ) 202>./.processnewblock.lock
 }
 
-bitcoin_node_newblock
+bitcoin_node_blocknotify
 returncode=$?
-trace "[bitcoin_node_newblock] exiting"
+trace "[bitcoin_node_blocknotify] exiting"
 exit ${returncode}
