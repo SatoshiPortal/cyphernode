a6c6cc83cb274797ad8fb7d19f41bea399accc8a
Philippe Lamy <philippe@philippelamy.ca>
2022-06-14 12:59:05 -0400

    rebase - Changes after CR + doc updates

--- cyphernodeconf_docker/templates/bitcoin/blocknotify.sh
+++ cyphernodeconf_docker/templates/bitcoin/blocknotify.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
 blocknotify(){
-  local pid=$(cut -d' ' -f4 < /proc/self/stat)
-
-  echo "[blocknotify-$pid] Entering blocknotify"
+  echo "[blocknotify-$$] Entering blocknotify"
 
   local blockhash="$@"
-  echo "[blocknotify-$pid] [blockhash=$blockhash]"
+  echo "[blocknotify-$$] [blockhash=$blockhash]"
 
   local blockheight
   blockheight=$(get_block_height $blockhash)
 
-  echo "[blocknotify-$pid] mosquitto_pub -h broker --retain -t newblock -m \"{\"blockhash\":\"${blockhash}\",\"blockheight\":${blockheight}}\""
+  echo "[blocknotify-$$] mosquitto_pub -h broker --retain -t newblock -m \"{\"blockhash\":\"${blockhash}\",\"blockheight\":${blockheight}}\""
   mosquitto_pub -h broker --retain -t newblock -m "{\"blockhash\":\"${blockhash}\",\"blockheight\":${blockheight}}"
 
-  echo "[blocknotify-$pid] Done"
+  echo "[blocknotify-$$] Done"
 }
 
 get_block_height(){
