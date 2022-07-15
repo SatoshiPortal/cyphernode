e98f0d0dc0899b3d8b6831d51d2161bfa6261e9c
Philippe Lamy <philippe@philippelamy.ca>
2022-07-15 12:53:34 -0400

    Added dbl quotes to parameters for dash

--- cyphernodeconf_docker/templates/bitcoin/walletnotify.sh
+++ cyphernodeconf_docker/templates/bitcoin/walletnotify.sh
@@ -10,13 +10,13 @@ walletnotify(){
   for wallet in $(bitcoin-cli listwallets | grep watching | tr -d ,\")
   do
     echo "[walletnotify-$$] tx=(bitcoin-cli -rpcwallet=$wallet gettransaction $txid true true)"
-    tx=$(bitcoin-cli -rpcwallet=$wallet gettransaction $txid true true)
-    tx=$(echo $tx | jq -Mc)
+    tx=$(bitcoin-cli -rpcwallet="$wallet" gettransaction "$txid" true true)
+    tx=$(echo "$tx" | jq -Mc)
 
     if [ -n "$tx" ]; then
       echo "[walletnotify-$$] Found [$txid] in wallet [$wallet]"
       echo "[walletnotify-$$] mosquitto_pub -h broker -t bitcoin_watching_walletnotify -m \"$tx\" "
-      mosquitto_pub -h broker -t bitcoin_watching_walletnotify -m $(echo $tx | base64 -w 0)
+      mosquitto_pub -h broker -t bitcoin_watching_walletnotify -m $(echo "$tx" | base64 -w 0)
       break;
     fi
   done
