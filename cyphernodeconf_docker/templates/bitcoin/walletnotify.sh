f9ab14e5dd9a134d932c0f91e5b6cc70817fd02e
Philippe Lamy <philippe@philippelamy.ca>
2022-07-15 15:55:56 -0400

    rebase - Redirect stderr to keep btc core log clean

--- cyphernodeconf_docker/templates/bitcoin/walletnotify.sh
+++ cyphernodeconf_docker/templates/bitcoin/walletnotify.sh
@@ -6,18 +6,22 @@ walletnotify(){
   local txid="$@"
   echo "[walletnotify-$$] [txid=$txid]"
   local tx
+  local error
 
   for wallet in $(bitcoin-cli listwallets | grep watching | tr -d ,\")
   do
     echo "[walletnotify-$$] tx=(bitcoin-cli -rpcwallet=$wallet gettransaction $txid true true)"
-    tx=$(bitcoin-cli -rpcwallet="$wallet" gettransaction "$txid" true true)
-    tx=$(echo "$tx" | jq -Mc)
-
-    if [ -n "$tx" ]; then
-      echo "[walletnotify-$$] Found [${txid}] in wallet [${wallet}]"
+    tx=$(bitcoin-cli -rpcwallet="$wallet" gettransaction "$txid" true true 2>&1)
+    error=$(echo ${tx} | grep 'error')
+    
+    if [ -z "${error}" ]; then
+      tx=$(echo "$tx" | jq -Mc)
+      echo "[walletnotify-$$] Found ["$txid"] in wallet ["$wallet"]"
       echo "[walletnotify-$$] mosquitto_pub -h broker -t bitcoin_watching_walletnotify -m \"$tx\" "
       mosquitto_pub -h broker -t bitcoin_watching_walletnotify -m $(echo "$tx" | base64 -w 0)
       break;
+    else
+      echo "[walletnotify-$$] Did not find ["$txid"] in wallet ["$wallet"] : ${error}"
     fi
   done
 
