3f86c4271b82bc3eb5c2813e9633f62f636a6c74
Michael <michael@wrtdesign.net>
2025-02-22 04:06:24 +1100

    bitcoinnode/walletnotify topic outside watching conditional (#342)
    * bitcoinnode/walletnotify topic outside watching conditional. Del unnecessary break
    
    * Added a comment to clear things up
    
    * Let's make it right
    
    * Apply same fix to elements
    
    ---------
    
    Co-authored-by: kexkey <github@kexkey.com>

--- cyphernodeconf_docker/templates/elements/walletnotify.sh
+++ cyphernodeconf_docker/templates/elements/walletnotify.sh
@@ -3,38 +3,42 @@
 walletnotify(){
   echo "[walletnotify-$$] Entering walletnotify"
 
-  local txid="$1"
-  echo "[walletnotify-$$] [txid=$txid]"
-  local walletname="$2"
-  echo "[walletnotify-$$] [walletname=$walletname]"
+  local txid="${1}"
+  echo "[walletnotify-$$] [txid=${txid}]"
+  local walletname="${2}"
+  echo "[walletnotify-$$] [walletname=${walletname}]"
   local tx
   local error
   local watching_wallet
 
-  if [ "${walletname}" = "watching01.dat" ] || [ "${walletname}" = "xpubwatching01.dat" ] || [ "${walletname}" = "spending01.dat" ]; then
-    # Let's fetch transaction data in the spending wallet because there's too much missing information in the watching wallet for liquid transactions.
-    # If we were watching an external address that is not in the spending wallet, too bad, it's not supported for now.
-    echo "[walletnotify-$$] tx=(elements-cli -rpcwallet=spending01.dat gettransaction $txid true true)"
-    tx=$(elements-cli -rpcwallet="spending01.dat" gettransaction "$txid" true true 2>&1)
-    error=$(echo ${tx} | grep 'error')
+  # The Elements node may have several other wallets used for other purposes than the ones by Cyphernode, so
+  # we need to filter out the transactions from those other wallets before publishing them to the Cyphernode MQTT topics.
 
-    if [ -z "${error}" ]; then
-      tx=$(echo "$tx" | jq -Mc)
-      echo "[walletnotify-$$] Found ["$txid"] in wallet ["$walletname"]"
-      watching_wallet=$(echo $walletname | grep watching)
+  # We are using the cyphernode/elements/walletnotify topic for Cyphernode purposes (watcher, confirmation management, etc.) and
+  # using the elementsnode/walletnotify topic for other purposes, like the cypherapps that are subscribed to it on the broker.
+  # We are only publishing transactions useful for Cyphernode on the Cyphernode's topic and all of them to the Cypherapps one.
 
-      if [ -n "${watching_wallet}" ]; then
-        echo "[walletnotify-$$] It's a watching wallet ["$walletname"] - Adding topic cyphernode/elements/walletnotify"
-        echo "[walletnotify-$$] mosquitto_pub -h broker -t cyphernode/elements/walletnotify -m \"$tx\" "
-        mosquitto_pub -h broker -t cyphernode/elements/walletnotify -m $(echo $tx | base64 -w 0)
+  # Let's fetch transaction data in the spending wallet because there's too much missing information in the watching wallet for liquid transactions.
+  # If we were watching an external address that is not in the spending wallet, it's too bad, it's not supported for now.
+  echo "[walletnotify-$$] tx=(elements-cli -rpcwallet=spending01.dat gettransaction ${txid} true true)"
+  tx=$(elements-cli -rpcwallet="spending01.dat" gettransaction "${txid}" true true 2>&1)
+  error=$(echo ${tx} | grep 'error')
 
-        echo "[walletnotify-$$] mosquitto_pub -h broker -t elementsnode/walletnotify -m \"$tx\" "
-        mosquitto_pub -h broker -t elementsnode/walletnotify -m $(echo $tx | base64 -w 0)
-      fi
-      break;
-    else
-      echo "[walletnotify-$$] Did not find ["$txid"] in wallet ["$walletname"] : ${error}"
+  if [ -z "${error}" ]; then
+    echo "[walletnotify-$$] Found ["$txid"] in wallet ["$walletname"]"
+    tx=$(echo "${tx}" | jq -Mc)
+    txb64=$(echo ${tx} | base64 -w 0)
+
+    if [ "${walletname}" = "watching01.dat" ] || [ "${walletname}" = "xpubwatching01.dat" ]; then
+      echo "[walletnotify-$$] It's a watching wallet ["${walletname}"] - Adding topic cyphernode/elements/walletnotify"
+      echo "[walletnotify-$$] mosquitto_pub -h broker -t cyphernode/elements/walletnotify -m \"${tx}\" "
+      mosquitto_pub -h broker -t cyphernode/elements/walletnotify -m "${txb64}"
     fi
+
+    echo "[walletnotify-$$] mosquitto_pub -h broker -t elementsnode/walletnotify -m \"${tx}\" "
+    mosquitto_pub -h broker -t elementsnode/walletnotify -m "${txb64}"
+  else
+    echo "[walletnotify-$$] Did not find ["$txid"] in wallet ["${walletname}"] : ${error}"
   fi
 
   echo "[walletnotify-$$] Done"
