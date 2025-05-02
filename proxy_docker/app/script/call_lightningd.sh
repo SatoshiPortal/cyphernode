7a08beacd4393dbd54310a9a9ef01095b1987a41
talej <michael@wrtdesign.net>
2025-05-02 10:58:41 +1000

    already_paid flag in pending case for ln_pay

--- proxy_docker/app/script/call_lightningd.sh
+++ proxy_docker/app/script/call_lightningd.sh
@@ -379,7 +379,10 @@ ln_pay() {
       fi
       if [ "${pending}" != "null" ]; then
         trace "[ln_pay] responding pending"
-        echo "${pending}"
+
+        # Add the "already_paid" flag to indicate this was a previously paid invoice
+        local already_paid=$(echo "${pending}" | jq '. + {"already_paid": true}')
+        echo "${already_paid}"
         return 1
       fi
 
